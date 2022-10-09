2b54d3a7172baf6e757094ba7c736c64613087fc
Avirup-301294 <avirup_49>
2022-10-09 09:02:43 +0530

    refactoring to common base interface. closes#1

--- pet-clinic-data/src/main/java/com/avirup/petclinicdata/services/PetService.java
+++ pet-clinic-data/src/main/java/com/avirup/petclinicdata/services/PetService.java
@@ -1,11 +1,10 @@
 package com.avirup.petclinicdata.services;
 
+import com.avirup.petclinicdata.model.Owner;
 import com.avirup.petclinicdata.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
+
 }
